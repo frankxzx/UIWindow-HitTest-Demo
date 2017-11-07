fe9c0f5063b9b8aac4f8d6b11a2eaa5c7cb36698
frank.xu <frank.xu@touch-spring.com>
2017-11-07 23:04:42 +0800

    Initial Commit

---
+++ UIWindow-HitTest-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIWindow-HitTest-Demo
+//
+//  Created by Xuzixiang on 2017/11/7.
+//  Copyright © 2017年 frankxzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIWindow-HitTest-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIWindow-HitTest-Demo
+//
+//  Created by Xuzixiang on 2017/11/7.
+//  Copyright © 2017年 frankxzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
